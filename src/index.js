aa590ebd995b740035442eb7994195b9539e5dd8
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-09-09 15:50:38 +0300

    Add full hw

--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/countriesAPI';
 import { countriesListMarkup } from './js/countriesMarkup';
+import { countriesInfoMarkup } from './js/countriesMarkup';
 
 const DEBOUNCE_DELAY = 300;
 const refCountryList = document.querySelector('.country-list');
+const refCountryInfo = document.querySelector('.country-info');
+const refInput = document.querySelector('#search-box');
 
-fetchCountries('ukr').then(resp => {
-  console.log(resp);
-  refCountryList.innerHTML = countriesListMarkup(resp);
-});
+refInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(e) {
+  let name = '';
+  name = e.target.value.trim();
+  if (name === '') {
+    infoReset();
+    return Notiflix.Notify.info('Please enter a name of country.');
+  }
+  fetchCountries(name).then(resp => {
+    if (resp === undefined) {
+      return;
+    }
+    if (resp.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+      infoReset();
+    }
+    if (resp.length <= 10 && resp.length > 2) {
+      refCountryList.innerHTML = countriesListMarkup(resp);
+      refCountryInfo.innerHTML = '';
+    }
+    if (resp.length === 1) {
+      refCountryInfo.innerHTML = countriesInfoMarkup(resp);
+      refCountryList.innerHTML = '';
+    }
+  });
+}
+
+function infoReset() {
+  refCountryInfo.innerHTML = '';
+  refCountryList.innerHTML = '';
+}
--- src/js/countriesMarkup.js
+++ src/js/countriesMarkup.js
@@ -2,10 +2,35 @@ export function countriesListMarkup(arr) {
   const markup = arr.reduce(
     (acc, { flags, name }) =>
       acc +
-      `<li class="country-item">
-      <img src=${flags.svg} alt="flag of the country" width="60">
+      `<li class="country-list__item">
+      <img src=${flags.svg} alt="flag of the country" width="100">
       <h3>${name.official}</h3>
       </li>`,
+    ''
+  );
+  return markup;
+}
+
+export function countriesInfoMarkup(arr) {
+  const markup = arr.reduce(
+    (acc, { name, capital, population, flags, languages }) => {
+      return (
+        acc +
+        ` <div class="">
+      <div class="country-info__title">
+        <img src="${
+          flags.svg
+        }" alt="flag of the country" width="40" height="30"/>
+        <h2>${name.official}</h2>
+      </div>
+      <p class="coutry-info__item"><span class="country-info__focus">Capital: </span> ${capital}</p>
+      <p class="coutry-info__item"><span class="country-info__focus">Population: </span>${population}</p>
+      <p class="coutry-info__item"><span class="country-info__focus">Languages: </span>${Object.values(
+        languages
+      )}</p>
+    </div>`
+      );
+    },
     ''
   );
   return markup;
